f8641c030f120edb4b2101eac0dce59fdbedbcb4
Pratik-Nawale <pratiknawale03@gmail.com>
2023-08-20 21:55:32 +0530

    Update Supply Chain

--- src/components/Supply Chain/SupplyChain.jsx
+++ src/components/Supply Chain/SupplyChain.jsx
@@ -3,14 +3,21 @@ import "./SupplyChain.css";
 
 const ChainContainer = () => {
   return (
-    <div class="supply-chain-image-container">
-      <img src="https://ninjasfiles.s3.amazonaws.com/asset_0000000000000025_1550237330_codezen_2.png" />
-      <div class="supply-chain-details">
-        <p>
-          Few research papers of parts of webinar related to new technology
-          development in supply chain.
-        </p>
+    <div class="supply-chain-outer-container">
+        <div class="card-title">Title line</div>
+      <div class="supply-chain-image-container">
+        <img src="https://www.kinaxis.com/sites/default/files/styles/max_650x650_webp/public/resources/Kinaxis-Concurrency%20is%20key%20for%20todays%20complex%20supply%20chains.webp?itok=KbCTin_n" />
+        <div class="supply-chain-details">
+          <p>
+            Few research papers of parts of webinar related to new technology
+            development in supply chain.
+          </p>
+        </div>
       </div>
+      <p>
+        Few research papers of parts of webinar related to new technology
+        development in supply chain.
+      </p>
     </div>
   );
 };
