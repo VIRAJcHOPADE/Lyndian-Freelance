492db0a238f16460ec2fb325e8db0b182660d28a
Kapil <kapilsoni54768161@gmail.com>
2023-08-21 08:28:41 +0530

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Brands from "./Brands/Brands";
 import { SupplyChain } from "./Supply Chain/SupplyChain";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Lydian - Home</title>
+      </Helmet>
       <Brands />
       <SupplyChain />
     </>
