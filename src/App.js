049f71397ff2927a748beb38ca1e34c3704ea333
Kapil <kapilsoni54768161@gmail.com>
2023-08-26 11:59:53 +0530

    Made the contact us form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import ContactUs from "./components/ContactUs/ContactUs.jsx";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import PageUnderConstruction from "./components/PageUnderConstruction/PageUnderConstruction";
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact-us" element={<ContactUs />} />
         <Route path="*" element={<PageUnderConstruction />} />
       </Routes>
       <Footer />
