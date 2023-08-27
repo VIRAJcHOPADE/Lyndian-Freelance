f21976e64d1e87699ceb183affc8379c08b465f2
Kapil <kapilsoni54768161@gmail.com>
2023-08-27 15:25:11 +0530

    Done with the independent assessment and testing page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import PageUnderConstruction from "./components/PageUnderConstruction/PageUnderConstruction";
 import DesignAudit from "./components/Services/DesignAudit/DesignAudit";
+import IndependentAssessmentAndTesting from "./components/Services/IndependentAssessmentAndTesting/IndependentAssessmentAndTesting";
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/design-audit" element={<DesignAudit />} />
+        <Route
+          path="/independent-assessment-and-testing"
+          element={<IndependentAssessmentAndTesting />}
+        />
         <Route path="*" element={<PageUnderConstruction />} />
       </Routes>
       <Footer />
---
+++ src/components/Services/IndependentAssessmentAndTesting/IndependentAssessmentAndTesting.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./IndependentAssessmentAndTesting.css";
+
+const IndependentAssessmentAndTesting = () => {
+  return (
+    <>
+      <img
+        id="independent-assessment-and-testing-image"
+        src="https://themanagementtechnician.com/wp-content/uploads/2022/11/atest.jpg"
+        alt=""
+      />
+      <div id="independent-assessment-and-testing-content">
+        <img
+          src="https://elearningindustry.com/wp-content/uploads/2021/05/end-to-end-assessment-supporting-the-student-education-journey.png"
+          alt=""
+        />
+        <div className="independent-assessment-and-testing-data">
+          <h1>Independent Assessment And Testing</h1>
+          <span>
+            <p>
+              Lorem ipsum dolor sit amet. Qui provident rerum aut quae
+              doloremque sed atque veniam et voluptatem dolore? Qui fugit
+              aliquam aut dolorem illo ut dicta enim. Cum voluptas quis rem
+              facere quibusdam vel saepe assumenda sit eligendi voluptatem est
+              tempore quisquam vel aperiam sint ad nisi nihil? Et quia ratione
+              non voluptas sunt qui voluptatem officiis ad totam repellat aut
+              dolor labore.
+            </p>
+            <p>
+              Aut repellat illo et eveniet dolorem et voluptas consequatur 33
+              provident consequuntur in ullam dolorem ut commodi sint est omnis
+              minus. Est quod quia non quaerat ducimus non atque temporibus eum
+              iste nemo sit dolorem sunt id vitae quidem.
+            </p>
+            <p>
+              Hic laboriosam mollitia nam beatae quis eos accusamus earum. Et
+              assumenda quidem quo assumenda laboriosam sit rerum dolores ut
+              suscipit repudiandae eos molestiae itaque.
+            </p>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default IndependentAssessmentAndTesting;
