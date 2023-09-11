7e58b0aea20d05a51615d39b9e14b27f4590e7a0
Kapil <kapilsoni54768161@gmail.com>
2023-09-11 19:22:17 +0530

    Added more slides to the home page slider

--- src/components/Home/Slider/SliderCont.jsx
+++ src/components/Home/Slider/SliderCont.jsx
@@ -16,25 +16,60 @@ const SliderCont = () => {
     },
     {
       image:
-        "https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-      title: "Are your Supply and Demand plans sensible?",
+        "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title: "Are your Supply and Demand plans sensible ?",
     },
     {
       image:
-        "https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-      title: "What if the price of Raw materials serges by 40%",
+        "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80",
+      title: "What if the price of Raw materials serges by 40% ?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80",
+      title:
+        "Why should you produce in the first place if you can't even sell at the price you thought you would ?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1512295767273-ac109ac3acfa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
+      title:
+        "How much to buy and when to buy is often strategic discussion, your ROP/ROQ/MOQ etc.. are just gimmicks ?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+      title:
+        "Did you know that the risk of sudden price change can turn your S&OP upside down ?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1544639160-ea6a810ab181?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+      title:
+        "Why do you even forecast, When you can replenish automatically based on consumption rate ?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1614028674026-a65e31bfd27c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title: "What makes you think your inventory level is high ?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title:
+        "Are you using market research data in your market demand forecasting ?",
     },
     {
       image:
         "https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
       title:
-        "Why should you produce in the first place if you can't even sell at the price you thought you would.",
+        "How real are your lead times, do you even bother to update it based on reality ?",
     },
     {
       image:
-        "https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        "https://images.unsplash.com/photo-1488272690691-2636704d6000?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2067&q=80",
       title:
-        "How much to buy and when to buy is often strategic discussion, your ROP/ROQ/MOQ etc.. are just gimmicks. ",
+        "Do you know the in built algorithm in Supply Chain software you decide to buy ?",
     },
   ];
 
