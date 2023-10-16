877d6751a20dbaf11b33e1b160ce59583385d05c
unknown <virajchopade527@gmail.com>
2023-10-16 11:43:04 +0530

    Update team

--- src/components/Home/Team/team.jsx
+++ src/components/Home/Team/team.jsx
@@ -9,7 +9,7 @@ const team = () => {
         <div class="Theams-Waper">
           <div class="back-div">
             <div class="Innovativebrands">
-              <h2 class="theamheading">Our Customers</h2>
+              <h2 class="theamheading">Team</h2>
               <br />
             </div>
             <div class="tmain">
@@ -17,56 +17,38 @@ const team = () => {
 
                 <img
                   class="brandimage image1"
-                  src="./../../../public/Picture1.jpg"
+                  src="Picture1.jpg"
                 />
               </div>
 
               <div class="tcontent">
                 <img
                   class="brandimage"
-                  src="https://www.transparentpng.com/thumb/adidas-logo/hd-adidas-logo-clipart-3.png"
+                  src="Picture2.jpg"
                 />
               </div>
               <div class="tcontent">
                 <img
                   class="brandimage"
-                  src="https://www.jgsummit.com.ph/annualreport2019/wp-content/uploads/jgsummit-logo-horizontal.png"
+                  src="Picture3.jpg"
                 />
               </div>
               <div class="tcontent">
                 <img
                   class="brandimage"
-                  src="https://seeklogo.com/images/P/petro-rabigh-logo-0982F5EDFC-seeklogo.com.png"
+                  src="Picture4.jpg"
                 />
               </div>
               <div class="tcontent">
                 <img
                   class="brandimage"
-                  src="https://www.lycra.com/sites/lycra/files/2023-06/lycra-logo-brands-jM.png"
+                  src="Picture5.jpg"
                 />
               </div>
               <div class="tcontent">
                 <img
                   class="brandimage"
-                  src="https://seeklogo.com/images/U/upl-united-phosphorus-limited-logo-5F4673A2C1-seeklogo.com.png?v=638246735760000000"
-                />
-              </div>
-              <div class="tcontent">
-                <img
-                  class="brandimage"
-                  src="https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Glenmark_Pharmaceuticals_logo.png/1280px-Glenmark_Pharmaceuticals_logo.png"
-                />
-              </div>
-              <div class="tcontent">
-                <img
-                  class="brandimage"
-                  src="https://www.sabra.com/binaries/content/gallery/sabra/content/new_sabra_logo.jpg/new_sabra_logo.jpg/sabra%3Axlargeretina"
-                />
-              </div>
-              <div class="tcontent">
-                <img
-                  class="brandimage"
-                  src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Logo-Abdul-Latif-Jameel-co.png?20160414091944"
+                  src="Picture6.png"
                 />
               </div>
             </div>
