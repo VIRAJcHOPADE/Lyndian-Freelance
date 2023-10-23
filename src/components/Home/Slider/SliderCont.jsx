ad3824348a57758252c0468419b6888384478207
Kapil <kapilsoni54768161@gmail.com>
2023-10-23 16:24:09 +0530

    .

--- src/components/Home/Slider/SliderCont.jsx
+++ src/components/Home/Slider/SliderCont.jsx
@@ -104,7 +104,7 @@ const SliderCont = () => {
       setImage(elements[num].image);
       setTitle(elements[num].title);
       setInd(elements[num].ind);
-    }, 4500);
+    }, 7000);
     return () => clearInterval(timer);
   }, []);
   return (
